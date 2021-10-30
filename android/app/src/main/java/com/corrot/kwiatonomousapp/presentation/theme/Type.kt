ed3241714808ae3319fe174c538b124be8babcbc
gitcorrot <junlol123@gmail.com>
2021-10-30 16:25:43 +0200

    android/Improved typography

--- android/app/src/main/java/com/corrot/kwiatonomousapp/presentation/theme/Type.kt
+++ android/app/src/main/java/com/corrot/kwiatonomousapp/presentation/theme/Type.kt
@@ -2,27 +2,45 @@ package com.corrot.kwiatonomousapp.presentation.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.corrot.kwiatonomousapp.R
 
-// Set of Material typography styles to start with
+val RobotoFamily = FontFamily(
+    Font(R.font.roboto_regular, FontWeight.Normal),
+    Font(R.font.roboto_medium, FontWeight.Bold),
+    Font(R.font.roboto_thin, FontWeight.Thin),
+    Font(R.font.roboto_light, FontWeight.Light),
+    Font(R.font.roboto_italic, FontWeight.Normal, FontStyle.Italic)
+)
+
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+    h1 = TextStyle(
+        fontFamily = RobotoFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 30.sp
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
+    h2 = TextStyle(
+        fontFamily = RobotoFamily,
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontSize = 24.sp
+    ),
+    h3 = TextStyle(
+        fontFamily = RobotoFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp
+    ),
+    body1 = TextStyle(
+        fontFamily = RobotoFamily,
+        fontWeight = FontWeight.Light,
+        fontSize = 16.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = RobotoFamily,
+        fontWeight = FontWeight.Thin,
+        fontSize = 14.sp
     )
-    */
 )
