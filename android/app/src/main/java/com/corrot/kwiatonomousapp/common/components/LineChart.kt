4c9e2cef03fbb9e37d8407f0c052026112040e17
Julian Czaja <julianczaja97@gmail.com>
2021-11-24 21:04:15 +0100

    android/Improved line chart. Added preview.

--- android/app/src/main/java/com/corrot/kwiatonomousapp/common/components/LineChart.kt
+++ android/app/src/main/java/com/corrot/kwiatonomousapp/common/components/LineChart.kt
@@ -1,11 +1,15 @@
 package com.corrot.kwiatonomousapp.common.components
 
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.graphics.Paint
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.border
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -13,30 +17,102 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.corrot.kwiatonomousapp.common.mapBetween
+import com.corrot.kwiatonomousapp.presentation.theme.KwiatonomousAppTheme
+
+@Preview("Light", widthDp = 400, heightDp = 250, uiMode = UI_MODE_NIGHT_NO)
+@Composable
+fun LineChartPreviewLight() {
+    KwiatonomousAppTheme(darkTheme = false) {
+        Surface {
+            Box(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .height(250.dp)
+            ) {
+                lineChart(
+                    xData = listOf(1f, 2f, 3f, 4f, 10f, 15f),
+                    yData = listOf(10f, 12f, 13f, 14f, 10f, 15f),
+                    title = "Humidity",
+                )
+            }
+        }
+    }
+}
+
+@Preview("DARK", widthDp = 400, heightDp = 200, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun LineChartPreviewDark() {
+    KwiatonomousAppTheme(darkTheme = true) {
+        Surface {
+            Box(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .height(250.dp)
+            ) {
+                lineChart(
+                    xData = listOf(1f, 2f, 3f, 4f, 10f, 15f),
+                    yData = listOf(10f, 12f, 13f, 14f, 10f, 15f),
+                    title = "Temperature",
+                    yAxisUnit = "°C",
+                    renderDropLines = true,
+                    marginX = 0f,
+                    marginY = 0f
+                )
+            }
+        }
+    }
+}
 
 @Composable
 fun lineChart(
     xData: List<Float>,
     yData: List<Float>,
-    title: String,
+    title: String = "",
     marginX: Float = 50f,
     marginY: Float = 50f,
     lineColor: Color = MaterialTheme.colors.primary,
     circleColor: Color = MaterialTheme.colors.primary,
+    xAxisUnit: String = "",
     yAxisUnit: String = "",
+    xAxisDividersCount: Int = 5,
     yAxisDividersCount: Int = 4,
+    renderGridLines: Boolean = true,
+    renderDropLines: Boolean = false,
 ) {
+    val isDarkTheme = isSystemInDarkTheme()
+    val yAxisPaint = Paint().apply {
+        color = if (isDarkTheme) 0xFFFFFFFF.toInt() else 0xFF000000.toInt()
+        textSize = 24f
+        textAlign = Paint.Align.RIGHT
+    }
+    val xAxisPaint = Paint().apply {
+        color = if (isDarkTheme) 0xFFFFFFFF.toInt() else 0xFF000000.toInt()
+        textSize = 24f
+        textAlign = Paint.Align.CENTER
+    }
+    val axisDecorationColor = if (isDarkTheme) Color.White else Color.Gray
+
+    val xValueMax = xData.maxOrNull() ?: 0f
+    val xValueMin = xData.minOrNull() ?: 0f
+    val yValueMax = yData.maxOrNull() ?: 0f
+    val yValueMin = yData.minOrNull() ?: 0f
+
+    val axisPaddingX = 100f
+    val axisPaddingY = 50f
+
     Column(
         modifier = Modifier
             .fillMaxSize()
-//            .border(
-//                border = BorderStroke(0.5.dp, Color.White),
-//                shape = RectangleShape
-//            )
+            .border(
+                border = BorderStroke(0.5.dp, Color.White),
+                shape = RectangleShape
+            )
     ) {
         Text(
             text = title,
@@ -46,14 +122,12 @@ fun lineChart(
                 .align(Alignment.CenterHorizontally),
         )
         Canvas(Modifier.fillMaxSize()) {
-            val xMin = 100f + marginX
+            val xMin = axisPaddingX + marginX
             val xMax = size.width - marginX
             val yMin = marginY
-            val yMax = size.height - marginY
+            val yMax = size.height - marginY - axisPaddingY
 
             // X axis data
-            val xValueMax = xData.maxOrNull() ?: 0f
-            val xValueMin = xData.minOrNull() ?: 0f
             val xAxis = xData.map {
                 it.mapBetween(
                     xValueMin, xValueMax,
@@ -62,8 +136,6 @@ fun lineChart(
             }
 
             // Y axis data
-            val yValueMax = yData.maxOrNull() ?: 0f
-            val yValueMin = yData.minOrNull() ?: 0f
             val yAxis = yData.map {
                 it.mapBetween(
                     yValueMax, yValueMin,
@@ -74,52 +146,82 @@ fun lineChart(
 
             // X axis decoration
             drawLine(
-                color = Color.White,
-                start = Offset(100f, size.height),
-                end = Offset(size.width, size.height),
+                color = axisDecorationColor,
+                start = Offset(axisPaddingX, size.height - axisPaddingY),
+                end = Offset(size.width - marginX, size.height - axisPaddingY),
                 strokeWidth = 2f
             )
+            val xAxisValueStep = (xValueMax - xValueMin) / (xAxisDividersCount - 1)
+            for (i in 0 until xAxisDividersCount) {
+                val xCurrent = xValueMin + (xAxisValueStep * i)
+                val xAxisValue = xCurrent.mapBetween(
+                    xValueMin, xValueMax,
+                    xMin, xMax
+                )
+                drawLine(
+                    color = axisDecorationColor,
+                    start = Offset(xAxisValue, size.height - axisPaddingY - 10f),
+                    end = Offset(xAxisValue, size.height - axisPaddingY + 10f),
+                    strokeWidth = 2f
+                )
+                if (renderDropLines) {
+                    drawLine(
+                        color = axisDecorationColor,
+                        start = Offset(xAxisValue, yMin),
+                        end = Offset(xAxisValue, size.height - axisPaddingY),
+                        strokeWidth = 2f,
+                        alpha = 0.5f,
+                        pathEffect = PathEffect.dashPathEffect(floatArrayOf(12f, 12f))
+                    )
+                }
+                drawIntoCanvas {
+                    it.nativeCanvas.drawText(
+                        "%.1f%s".format(xCurrent, xAxisUnit),
+                        xAxisValue,
+                        size.height - axisPaddingY + 36f,
+                        xAxisPaint
+                    )
+                }
+            }
+
 
             // Y axis decoration
             drawLine(
-                color = Color.White,
-                start = Offset(100f, 0f),
-                end = Offset(100f, size.height),
+                color = axisDecorationColor,
+                start = Offset(axisPaddingX, yMin),
+                end = Offset(axisPaddingX, size.height - axisPaddingY),
                 strokeWidth = 2f
             )
-            val yAxisValueStep = (yValueMax - yValueMin) / yAxisDividersCount
-            val paint = Paint().apply {
-                color = 0xFFFFFFFF.toInt()
-                textSize = 24f
-                textAlign = Paint.Align.RIGHT
-            }
-            for (i in 0..yAxisDividersCount) {
+            val yAxisValueStep = (yValueMax - yValueMin) / (yAxisDividersCount - 1)
+            for (i in 0 until yAxisDividersCount) {
                 val yCurrent = yValueMin + (yAxisValueStep * i)
                 val yAxisValue = yCurrent.mapBetween(
                     yValueMax, yValueMin,
                     yMin, yMax
                 )
                 drawLine(
-                    color = Color.White,
-                    start = Offset(90f, yAxisValue),
-                    end = Offset(110f, yAxisValue),
+                    color = axisDecorationColor,
+                    start = Offset(axisPaddingX - 10f, yAxisValue),
+                    end = Offset(axisPaddingX + 10f, yAxisValue),
                     strokeWidth = 2f
                 )
-                drawLine(
-                    color = Color.White,
-                    start = Offset(110f, yAxisValue),
-                    end = Offset(size.width, yAxisValue),
-                    strokeWidth = 2f,
-                    alpha = 0.3f,
-                    pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f))
-                )
+                if (renderGridLines) {
+                    drawLine(
+                        color = axisDecorationColor,
+                        start = Offset(axisPaddingX + 20f, yAxisValue),
+                        end = Offset(size.width - marginX, yAxisValue),
+                        strokeWidth = 2f,
+                        alpha = 0.5f,
+                        pathEffect = PathEffect.dashPathEffect(floatArrayOf(12f, 12f))
+                    )
+                }
 
                 drawIntoCanvas {
                     it.nativeCanvas.drawText(
                         "%.1f%s".format(yCurrent, yAxisUnit),
-                        80f,
-                        yAxisValue - 2f,
-                        paint
+                        axisPaddingX - 24f,
+                        yAxisValue,
+                        yAxisPaint
                     )
                 }
             }
@@ -129,11 +231,11 @@ fun lineChart(
             for (i in xAxis.indices) {
                 if (i == 0) {
                     lastPoint = Offset(xAxis[0], yAxis[0])
-//                    drawCircle(
-//                        color = circleColor,
-//                        radius = 4f,
-//                        center = lastPoint
-//                    )
+                    drawCircle(
+                        color = circleColor,
+                        radius = 4f,
+                        center = lastPoint
+                    )
                 } else {
                     val newPoint = Offset(xAxis[i], yAxis[i])
                     drawLine(
@@ -142,11 +244,11 @@ fun lineChart(
                         color = lineColor,
                         strokeWidth = 2f
                     )
-//                    drawCircle(
-//                        color = circleColor,
-//                        radius = 4f,
-//                        center = newPoint
-//                    )
+                    drawCircle(
+                        color = circleColor,
+                        radius = 4f,
+                        center = newPoint
+                    )
                     lastPoint = newPoint.copy()
                 }
             }
