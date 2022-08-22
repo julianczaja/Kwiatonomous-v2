49c9a1406d2facebcac1bfd6a9d739e45e1fd00c
julianczaja <julianczaja97@gmail.com>
2022-08-22 11:55:11 +0200

    android/Implemented adding device low battery events

--- android/app/src/main/java/com/corrot/kwiatonomousapp/DeviceBatteryInfoWorker.kt
+++ android/app/src/main/java/com/corrot/kwiatonomousapp/DeviceBatteryInfoWorker.kt
@@ -7,8 +7,10 @@ import androidx.work.WorkerParameters
 import com.corrot.kwiatonomousapp.common.Constants.LOW_BATTERY_VOLTAGE_THRESHOLD
 import com.corrot.kwiatonomousapp.common.Constants.MAX_TIME_DIFF_HOURS
 import com.corrot.kwiatonomousapp.data.remote.dto.toDeviceUpdate
+import com.corrot.kwiatonomousapp.domain.model.DeviceEvent
 import com.corrot.kwiatonomousapp.domain.repository.DeviceUpdateRepository
 import com.corrot.kwiatonomousapp.domain.repository.UserRepository
+import com.corrot.kwiatonomousapp.domain.usecase.AddDeviceEventUseCase
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
 import kotlinx.coroutines.flow.first
@@ -24,6 +26,7 @@ class DeviceBatteryInfoWorker @AssistedInject constructor(
     private val authManager: AuthManager,
     private val userRepository: UserRepository,
     private val deviceUpdateRepository: DeviceUpdateRepository,
+    private val addDeviceEventUseCase: AddDeviceEventUseCase,
 ) : CoroutineWorker(appContext, workerParams) {
 
     companion object {
@@ -65,10 +68,39 @@ class DeviceBatteryInfoWorker @AssistedInject constructor(
                         deviceName = userDevice.deviceName,
                         notificationId = userDevice.deviceId.hashCode()
                     )
+                    addLowBatteryEvent(
+                        userDevice.deviceId,
+                        deviceUpdate.batteryVoltage,
+                        deviceUpdate.batteryLevel
+                    )
                 }
             }
         }
 
         return Result.success()
+    }
+
+    private suspend fun addLowBatteryEvent(
+        deviceId: String,
+        batteryVoltage: Float,
+        batteryLevel: Int,
+    ) {
+
+        val deviceEvent = DeviceEvent.LowBattery(
+            deviceId = deviceId,
+            timestamp = LocalDateTime.now(),
+            batteryVoltage = batteryVoltage,
+            batteryLevel = batteryLevel
+        )
+        addDeviceEventUseCase.execute(deviceEvent).collect { ret ->
+            when (ret) {
+                is com.corrot.kwiatonomousapp.common.Result.Loading ->
+                    Timber.d("addDeviceEventUseCase (Low battery) loading...")
+                is com.corrot.kwiatonomousapp.common.Result.Success ->
+                    Timber.d("addDeviceEventUseCase (Low battery) success")
+                is com.corrot.kwiatonomousapp.common.Result.Error ->
+                    Timber.e("addDeviceEventUseCase (Low battery) error (${ret.throwable.message})")
+            }
+        }
     }
 }
