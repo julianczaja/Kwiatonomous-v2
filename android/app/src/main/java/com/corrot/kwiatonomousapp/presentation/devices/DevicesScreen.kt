5ac1c11edeb68869219d96271c61858c854087fb
julianczaja <julianczaja97@gmail.com>
2023-02-10 15:08:11 +0100

    android/Fixed preloading old data on devices screen

--- android/app/src/main/java/com/corrot/kwiatonomousapp/presentation/devices/DevicesScreen.kt
+++ android/app/src/main/java/com/corrot/kwiatonomousapp/presentation/devices/DevicesScreen.kt
@@ -1,5 +1,6 @@
 package com.corrot.kwiatonomousapp.presentation.devices
 
+import android.content.res.Configuration
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
@@ -11,6 +12,7 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
+import androidx.compose.material.Surface
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
@@ -21,6 +23,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -33,6 +36,8 @@ import com.corrot.kwiatonomousapp.domain.model.DeviceUpdate
 import com.corrot.kwiatonomousapp.domain.model.KwiatonomousAppState
 import com.corrot.kwiatonomousapp.domain.model.UserDevice
 import com.corrot.kwiatonomousapp.presentation.Screen
+import com.corrot.kwiatonomousapp.presentation.theme.KwiatonomousAppTheme
+import java.time.LocalDateTime
 
 @ExperimentalMaterialApi
 @ExperimentalFoundationApi
@@ -84,7 +89,9 @@ fun DevicesScreenContent(
 ) {
     val refreshState = rememberPullRefreshState(isLoading, refreshData)
 
-    Box(modifier = Modifier.fillMaxSize()) {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
         LazyVerticalGrid(
             columns = GridCells.Adaptive(minSize = 150.dp),
             contentPadding = PaddingValues(vertical = 8.dp, horizontal = 6.dp),
@@ -118,5 +125,56 @@ fun DevicesScreenContent(
             state = refreshState,
             modifier = Modifier.align(Alignment.TopCenter)
         )
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Composable
+private fun DevicesScreenContentLightPreview() {
+    KwiatonomousAppTheme(darkTheme = false) {
+        Surface {
+            DevicesScreenContent(
+                isLoading = true,
+                error = null,
+                userDevicesWithLastUpdates = listOf(
+                    Pair(
+                        UserDevice(
+                            deviceId = "deviceId",
+                            deviceName = "Device 1",
+                            deviceImageName = "flower_1",
+                            isFavourite = false,
+                            notificationsOn = false
+                        ),
+                        DeviceUpdate(
+                            deviceId = "deviceId",
+                            updateTime = LocalDateTime.of(2023, 2, 10, 14, 0, 0),
+                            batteryLevel = 55,
+                            batteryVoltage = 3.6f,
+                            temperature = 22.4f,
+                            humidity = 77.4f
+                        )
+                    ),
+                    Pair(
+                        UserDevice(
+                            deviceId = "deviceId2",
+                            deviceName = "Device 2",
+                            deviceImageName = "flower_2",
+                            isFavourite = false,
+                            notificationsOn = false
+                        ),
+                        DeviceUpdate(
+                            deviceId = "deviceId2",
+                            updateTime = LocalDateTime.of(2023, 2, 10, 14, 0, 0),
+                            batteryLevel = 55,
+                            batteryVoltage = 3.6f,
+                            temperature = 22.4f,
+                            humidity = 77.4f
+                        )
+                    )
+                ),
+                {}, {}, {}
+            )
+        }
     }
 }
--- android/app/src/main/java/com/corrot/kwiatonomousapp/presentation/devices/DevicesViewModel.kt
+++ android/app/src/main/java/com/corrot/kwiatonomousapp/presentation/devices/DevicesViewModel.kt
@@ -3,6 +3,8 @@ package com.corrot.kwiatonomousapp.presentation.devices
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.corrot.kwiatonomousapp.common.Result
+import com.corrot.kwiatonomousapp.domain.model.DeviceUpdate
+import com.corrot.kwiatonomousapp.domain.model.UserDevice
 import com.corrot.kwiatonomousapp.domain.usecase.GetUserDevicesWithLastUpdatesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
@@ -13,6 +15,8 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.cancellable
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
 import javax.inject.Inject
 
 @HiltViewModel
@@ -35,13 +39,27 @@ class DevicesViewModel @Inject constructor(
         getDataJob = viewModelScope.launch(ioDispatcher) {
             getUserDevicesWithLastUpdatesUseCase.execute().cancellable().collect { ret ->
                 when (ret) {
-                    is Result.Loading -> _state.update { it.copy(isLoading = true, userDevicesWithLastUpdates = ret.data) }
-                    is Result.Success -> _state.update { it.copy(isLoading = false, userDevicesWithLastUpdates = ret.data) }
-                    is Result.Error -> _state.update { it.copy(isLoading = false, error = ret.throwable.message ?: "Unknown error") }
+                    is Result.Loading -> _state.update {
+                        it.copy(isLoading = true, userDevicesWithLastUpdates = filterNewUpdates(ret.data))
+                    }
+                    is Result.Success -> _state.update {
+                        it.copy(isLoading = false, userDevicesWithLastUpdates = ret.data)
+                    }
+                    is Result.Error -> _state.update {
+                        it.copy(isLoading = false, error = ret.throwable.message ?: "Unknown error")
+                    }
                 }
             }
         }
     }
 
     fun confirmError() = _state.update { it.copy(error = null) }
+
+    private fun filterNewUpdates(data: List<Pair<UserDevice, DeviceUpdate?>>?) = data?.map {
+        // Preload only relatively new updates
+        val hoursSinceLastUpdate = ChronoUnit.HOURS.between(
+            it.second?.updateTime, LocalDateTime.now()
+        )
+        return@map Pair(it.first, if (hoursSinceLastUpdate <= 1) it.second else null)
+    }
 }
